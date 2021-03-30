5337dd1908888741139eb199b8ef983c0a38915c
felipedsmartins <tec.felipedsmartins@gmail.com>
2021-03-30 18:24:30 -0300

    Finished

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,54 @@
-import Head from 'next/head'
+import React, { Fragment } from 'react'
+import Head from 'next/head';
+import CardPricing from '../components/CardPricing';
 
-export default function Home() {
-  return (
-    <div className="w-full h-screen bg-gray-100 pt-8 px-3 sm:px-0 md:px-3 mt-24 border-t-8" style={{backgroundImage: "url('/img/granola.jpg')", backgroundSize:"900px", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundAttachment:"fixed"}}>
-  <section className="flex flex-col sm:flex-row justify-center -mt-24">
-    <article className="sm:flex-1 lg:flex-initial bg-white bg-opacity-50 lg:w-1/4 md:w-1/3 md:rounded-t-lg md:rounded-tr-none flex flex-col mt-0 sm:mt-8 border-t-8 border-gray-500 shadow-lg">
-      <img className="inline-block w-28 rounded-full ring-white" src="/img/etiquetaLogo.png" alt=""/>
-      <h3 className="text-center text-5xl font-bold p-5 pb-0 text-gray-600">245</h3>
-      <span className="text-center font-bold text-gray-600 ">unidades</span>
-      <ul className="text-center">
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Resistente a água</li>
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Próprio p/freezer</li>
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Recorte Eletrónico</li>
-      </ul>
-      <div className="text-center py-7 text-3xl mt-auto">
-         <span className="font-bold text-gray-900">R$86,00</span>
-       </div>
-      <div className="text-center my-8 mt-auto">
-        <a href="#" className="inline-block justify-center bg-green-600 hover:bg-gray-500 px-5 py-3 rounded text-white text-xl font-bold shadow-lg">Compre agora!</a>
-      </div>
-    </article>
-  
-     <article className="sm:flex-1 lg:flex-initial bg-white bg-opacity-50 lg:w-1/4 shadow-2xl z-30 md:rounded-t-lg mt-8 sm:mt-0 flex flex-col border-t-8 border-green-500 h-4/5">
-     <h3 className="text-center text-5xl font-bold p-5 pb-0 text-green-600">539</h3>
-      <span className="text-center font-bold text-green-600 ">unidades</span>
-      <ul className="text-center">
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Resistente a água</li>
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Próprio p/freezer</li>
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Recorte Eletrónico</li>
-      </ul>
-      <div className="text-center py-7 text-3xl mt-auto">
-         <span className="font-bold text-gray-900">R$119,00</span>
-       </div>
-       <div className="text-center my-8 mt-auto">
-        <a href="#" className="inline-block justify-center bg-green-600 hover:bg-gray-500 px-5 py-3 rounded text-white text-xl font-bold shadow-lg">Compre agora!</a>
-      </div>
-    </article>
-    
-     <article className="sm:flex-1 lg:flex-initial bg-white bg-opacity-50 lg:w-1/4 md:rounded-t-lg md:rounded-tl-none flex flex-col mt-8 border-t-8 md:border-t-8 border-indigo-500 shadow-lg">
-     <h3 className="text-center text-5xl font-bold p-5 pb-0 text-blue-600">1029</h3>
-      <span className="text-center font-bold text-blue-600 ">unidades</span>
-      <ul className="text-center">
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Resistente a água</li>
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Próprio p/freezer</li>
-        <li className="py-4 border-0 border-gray-light border-b border-solid">Recorte Eletrónico</li>
-      </ul>
-      <div className="text-center py-7 text-3xl mt-auto">
-         <span className="font-bold text-gray-900">R$175,00</span>
-       </div>
-       <div className="text-center my-8 mt-auto">
-        <a href="#" className="inline-block justify-center bg-green-600 hover:bg-gray-500 px-5 py-3 rounded text-white text-xl font-bold shadow-lg">Compre agora!</a>
-      </div>
-    </article>
-  </section>
-</div>
-  )
+const index = () => {
+    return (
+      <Fragment>
+        <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#3C4D9B" />
+        <meta name="description" content="Etiquetas e rotúlos personalizados com a melhor qualidade da impressão digital."/>
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
+        <title>Etiquetas e rótulos</title>
+        </Head>
+        <div className="container max-w-full" style={{ backgroundImage: "url('../img/granola.jpg')", backgroundSize:"900px", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundAttachment:"fixed"}}>
+          <section> 
+            <div className="container mx-auto ">
+              <h1 className="text-center text-5xl text-blue-900 font-medium leading-snug tracking-wider font-serif py-0 " style={{fontFamily: "Pacifico, cursive"}}>
+                Etiquetas Adesivas
+              </h1>
+              <p className="text-center  font-medium text-lg text-blue-900 shadow-xl mt-0 px-6 bg-white bg-opacity-70">
+                4cm de diâmetro (redondinha) em Bopp.
+              </p>
+              <div className="flex flex-wrap justify-center justify-around">
+              <CardPricing 
+                style={"w-11/12 max-w-md sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4 m-8"}
+                styleContainer={"text-sm leading-none rounded-t-lg bg-red-500 text-black font-semibold uppercase py-4 text-center tracking-wide"}
+                unidade={"245"} 
+                valor={"86,00"}/>
+              <CardPricing 
+                style={"w-11/12 max-w-md sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4 m-8"}
+                styleContainer={"text-sm leading-none rounded-t-lg bg-yellow-500 text-black font-semibold uppercase py-4 text-center tracking-wide"}
+                unidade={"539"} 
+                valor={"119,00"}/>
+              <CardPricing 
+                style={"w-11/12 max-w-md sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4 m-8"}
+                styleContainer={"text-sm leading-none rounded-t-lg bg-pink-400 text-black font-semibold uppercase py-4 text-center tracking-wide"}
+                unidade={"1029"} 
+                valor={"175,00"}/>  
+              </div>
+            </div>  
+          </section>
+        </div>
+      </Fragment>
+    )
 }
+
+export default index
+
+
+{/*w-11/12 max-w-md sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-10 bg-white rounded-lg shadow-lg*/}
